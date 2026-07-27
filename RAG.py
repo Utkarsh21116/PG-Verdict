7fa964a9bcb69181893c896ba89a8f8ab4b4e321
Utkarsh21116 <chaubeyutkarsh55@gmail.com>
2026-07-28 02:28:38 +0530

    RAG Pipeline Updated

--- RAG.py
+++ RAG.py
@@ -1,7 +1,13 @@
 import json
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+import os
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_chroma import Chroma
+from dotenv import load_dotenv
 
-with open('pg_essays\all_essays.json','r',encoding='utf-8') as f:
+load_dotenv()
+
+with open('pg_essays/all_essays.json','r',encoding='utf-8') as f:
     essays = json.load(f)
 
 text_splitter = RecursiveCharacterTextSplitter(
@@ -12,8 +18,24 @@ text_splitter = RecursiveCharacterTextSplitter(
 )
 
 document=[]
-count=0
+
 for essay in essays:
-    count+=1
-    print(essay['title'])
-print(count)
+    chunks = text_splitter.split_text(essay['text'])
+
+    for chunk in chunks:
+        document.append({
+            'page_content':chunk,
+            'metadata':{
+                'title':essay['title'],
+                'url':essay['url'],
+                "date": essay['date']
+            }
+        })
+
+embedding = GoogleGenerativeAIEmbeddings(model="gemini-embedding-001")
+vector_db = Chroma.from_texts(
+    texts=[doc["page_content"] for doc in document],
+    metadatas=[doc["metadata"] for doc in document],
+    embedding=embedding,
+    persist_directory="./pg_chroma_db"
+)
